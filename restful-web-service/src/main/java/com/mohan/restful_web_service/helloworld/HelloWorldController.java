d4e0ca47b3471625d2f1481088374cd4805f340e
Mohan Raj <mohanmwraj@gmail.com>
2025-09-21 10:48:05 +0530

    Controller-Basic-Implementation

---
+++ restful-web-service/src/main/java/com/mohan/restful_web_service/helloworld/HelloWorldController.java
@@ -0,0 +1,25 @@
+package com.mohan.restful_web_service.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping(path = "/hello-world")
+    public String helloWorld(){
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean(){
+        return new HelloWorldBean("Hello World");
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/request-test")
+    public String helloWorldRequestMapping(){
+        return "Request - Hello World";
+    }
+}
---
+++ restful-web-service/src/test/java/com/mohan/restful_web_service/RestfulWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohan.restful_web_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
