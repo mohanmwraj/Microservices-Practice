1150dbaad281936d9a828e3227f6f81ffe875d26
Mohan Raj <mohanmwraj@gmail.com>
2025-09-21 12:03:03 +0530

    Added Custom Exception

--- restful-web-service/src/main/java/com/mohan/restful_web_service/user/User.java
+++ restful-web-service/src/main/java/com/mohan/restful_web_service/user/User.java
@@ -8,6 +8,7 @@ public class User {
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
+        super();
         this.id = id;
         this.name = name;
         this.birthDate = birthDate;
